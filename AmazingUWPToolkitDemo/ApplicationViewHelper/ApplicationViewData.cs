6ea97e89024347ab6100112099cfc6c2c04ebd36
Timur Khamidov <khamitimur@live.ru>
2018-08-10 22:25:46 +0300

    Add demo project

---
+++ AmazingUWPToolkitDemo/ApplicationViewHelper/ApplicationViewData.cs
@@ -0,0 +1,42 @@
+﻿using AmazingUWPToolkit.ApplicatonView;
+using Windows.Foundation;
+
+namespace AmazingUWPToolkitDemo
+{
+    public class ApplicationViewData : IApplicationViewData
+    {
+        #region Implementation of IApplicationViewData
+
+        public string BackgroundColorResource => "TitleBarBackgroundColor";
+
+        public string InactiveBackgroundColorResource => "TitleBarBackgroundColor";
+
+        public string ForegroundColorResource => "TitleBarForegroundColor";
+
+        public string InactiveForegroundColorResource => "TitleBarDisabledForegroundColor";
+
+        public string ButtonBackgroundColorResource => "TitleBarButtonBackgroundColor";
+
+        public string ButtonHoverBackgroundColorResource => "TitleBarButtonHoverColor";
+
+        public string ButtonPressedBackgroundColorResource => "TitleBarButtonPressedColor";
+
+        public string ButtonInactiveBackgroundColorResource => "TitleBarButtonInactiveBackgroundColor";
+
+        public string ButtonForegroundColorResource => "TitleBarForegroundColor";
+
+        public string ButtonHoverForegroundColorResource => "TitleBarForegroundColor";
+
+        public string ButtonPressedForegroundColorResource => "TitleBarForegroundColor";
+
+        public string ButtonInactiveForegroundColorResource => "TitleBarDisabledForegroundColor";
+
+        public bool ExtendIntoTitleBar => true;
+
+        public Size? PreferredMinSize => new Size(1000, 1000);
+
+        public bool ConvertPreferredMinSizeUsingRawPixels => true;
+
+        #endregion
+    }
+}
