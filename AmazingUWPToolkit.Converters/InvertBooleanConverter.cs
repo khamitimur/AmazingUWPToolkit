6049c006704f76f5c62c390e53e80b05dfd40a19
Timur Khamidov <khamitimur@live.ru>
2018-08-18 18:43:55 +0300

    Add comments to InvertBooleanConverter

--- AmazingUWPToolkit.Converters/InvertBooleanConverter.cs
+++ AmazingUWPToolkit.Converters/InvertBooleanConverter.cs
@@ -3,10 +3,14 @@ using Windows.UI.Xaml.Data;
 
 namespace AmazingUWPToolkit.Converters
 {
+    /// <summary>
+    /// Converts <see cref="bool"/> value to it's inverted value.
+    /// </summary>
     public class InvertBooleanConverter : IValueConverter
     {
         #region Implementation of IValueConverter
 
+        /// <inheritdoc />
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             if (value is bool boolValue)
@@ -17,6 +21,7 @@ namespace AmazingUWPToolkit.Converters
             return null;
         }
 
+        /// <inheritdoc />
         public object ConvertBack(object value, Type targetType, object parameter, string language)
         {
             throw new NotImplementedException();
