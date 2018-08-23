678aa91cfee0afb1fbb9180bd837ed917af240c6
Timur Khamidov <baton237@gmail.com>
2018-08-23 19:33:04 +0300

    Add GazeButton

---
+++ AmazingUWPToolkit.Gaze/Controls/IGazeControl.cs
@@ -0,0 +1,17 @@
+﻿namespace AmazingUWPToolkit.Gaze.Controls
+{
+    public interface IGazeControl
+    {
+        #region Methods
+
+        void OnGazeEntered();
+
+        void OnGazeExited();
+
+        void OnGazeOverProgressChanged(double progress);
+
+        void OnGazeActionRequested();
+
+        #endregion
+    }
+}
--- AmazingUWPToolkitDemo/MainPage.xaml.cs
+++ AmazingUWPToolkitDemo/MainPage.xaml.cs
@@ -59,5 +59,10 @@ namespace AmazingUWPToolkitDemo
         #endregion
 
         #endregion
+
+        private void GazeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
