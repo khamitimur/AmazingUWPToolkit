b6edebed9e4216eab0afb10cfd7877d53d09f493
Timur Khamidov <khamitimur@live.ru>
2018-08-27 00:45:29 +0300

    Improve GazeTracker

--- AmazingUWPToolkit.Gaze/GazeTracker/GazeTracker.cs
+++ AmazingUWPToolkit.Gaze/GazeTracker/GazeTracker.cs
@@ -1,5 +1,6 @@
 ﻿using AmazingUWPToolkit.Gaze.Controls;
 using System;
+using System.Collections.Generic;
 using Windows.Devices.Input.Preview;
 using Windows.Foundation;
 using Windows.UI.Xaml;
@@ -18,20 +19,21 @@ namespace AmazingUWPToolkit.Gaze
         private static GazeDeviceWatcherPreview gazeDeviceWatcherPreview;
         private static GazeInputSourcePreview gazeInputSourcePreview;
 
-        private readonly IGazeControl gazeControl;
+        private readonly List<Control> controls;
+        private Control currentControlUnderGaze;
 
         private static DispatcherTimer timer;
         private double currentTimerValue;
-        private bool isFocused;
         private bool isGazeDwelledCalled;
+        private Point currentGazePoint;
 
         #endregion
 
         #region Contructor
 
-        public GazeTracker(IGazeControl gazeControl)
+        public GazeTracker()
         {
-            this.gazeControl = gazeControl;
+            controls = new List<Control>();
 
             gazeDeviceWatcherPreview = GazeInputSourcePreview.CreateWatcher();
 
@@ -45,6 +47,26 @@ namespace AmazingUWPToolkit.Gaze
         #endregion
 
         #region Overrides of IGazeTracker
+
+        public bool AddControl(Control control)
+        {
+            if (controls.Contains(control))
+                return false;
+
+            controls.Add(control);
+
+            return true;
+        }
+
+        public bool RemoveControl(Control control)
+        {
+            if (!controls.Contains(control))
+                return false;
+
+            controls.Remove(control);
+
+            return true;
+        }
 
         public void Start()
         {
@@ -73,10 +95,10 @@ namespace AmazingUWPToolkit.Gaze
 
         #region Static Methods
 
-        private static bool IsSupportedDevice(GazeDevicePreview gazeDevice)
+        private static bool IsSupportedDevice(GazeDevicePreview gazeDevicePreview)
         {
-            return gazeDevice.CanTrackEyes &&
-                gazeDevice.ConfigurationState == GazeDeviceConfigurationStatePreview.Ready;
+            return gazeDevicePreview.CanTrackEyes &&
+                gazeDevicePreview.ConfigurationState == GazeDeviceConfigurationStatePreview.Ready;
         }
 
         #endregion
@@ -128,60 +150,88 @@ namespace AmazingUWPToolkit.Gaze
             gazeInputSourcePreview.GazeMoved -= OnGazeInputSourcePreviewGazeMoved;
         }
 
-        Point lastHitPoint;
-
         private void OnGazeInputSourcePreviewGazeMoved(GazeInputSourcePreview sender, GazeMovedPreviewEventArgs args)
         {
-            if (gazeControl == null)
+            if (controls?.Count == 0)
                 return;
 
-            if (args.CurrentPoint.EyeGazePosition != null)
+            if (args.CurrentPoint.EyeGazePosition == null)
             {
-                double gazePointX = args.CurrentPoint.EyeGazePosition.Value.X;
-                double gazePointY = args.CurrentPoint.EyeGazePosition.Value.Y;
-
-                var gazePoint = new Point(gazePointX, gazePointY);
-
-                var doesHitControl = DoesElementContainPoint(gazePoint);
-
-                if (doesHitControl)
-                {
-                    lastHitPoint = gazePoint;
-
-                    if (isFocused)
-                        return;
-
-                    isFocused = true;
-
-                    gazeControl.OnGazeEntered();
-
-                    StartTimer();
-                }
-                else
-                {
-                    if (!isFocused)
-                        return;
-
-                    isFocused = false;
-
-                    StopTimer();
-
-                    gazeControl.OnGazeFixationProgressChanged(0);
-                    gazeControl.OnGazeExited();
-                }
-
-                args.Handled = true;
+                DiscardCurrentControlUnderGaze();
             }
+
+            var gazePointX = args.CurrentPoint.EyeGazePosition?.X;
+            var gazePointY = args.CurrentPoint.EyeGazePosition?.Y;
+
+            if (gazePointX == null || gazePointY == null)
+            {
+                DiscardCurrentControlUnderGaze();
+            }
+
+            var gazePoint = new Point(gazePointX.Value, gazePointY.Value);
+
+            var controlUnderGaze = FindControlUnderGaze(gazePoint);
+            if (controlUnderGaze == null)
+            {
+                DiscardCurrentControlUnderGaze();
+            }
+            else if (currentControlUnderGaze != null &&
+                     currentControlUnderGaze != controlUnderGaze)
+            {
+                DiscardCurrentControlUnderGaze();
+                SetControlUnderGaze(controlUnderGaze, gazePoint);
+            }
+            else if (currentControlUnderGaze == null)
+            {
+                SetControlUnderGaze(controlUnderGaze, gazePoint);
+            }
+
+            currentGazePoint = gazePoint;
+
+            args.Handled = true;
         }
 
-        private bool DoesElementContainPoint(Point gazePoint)
+        private void SetControlUnderGaze(Control control, Point gazePoint)
         {
-            var uielements = VisualTreeHelper.FindElementsInHostCoordinates(gazePoint, gazeControl as Control, true);
-            foreach (var uielement in uielements)
+            currentControlUnderGaze = control;
+
+            (currentControlUnderGaze as IGazeControl).OnGazeEntered();
+
+            StartTimer();
+        }
+
+        private void DiscardCurrentControlUnderGaze()
+        {
+            StopTimer();
+
+            (currentControlUnderGaze as IGazeControl)?.OnGazeFixationProgressChanged(0);
+            (currentControlUnderGaze as IGazeControl)?.OnGazeExited();
+
+            currentControlUnderGaze = null;
+        }
+
+        private Control FindControlUnderGaze(Point gazePoint)
+        {
+            for (var i = 0; i < controls.Count; i++)
             {
-                if (uielement is FrameworkElement frameworkElement)
+                var control = controls[i];
+                if (IsControlUnderGaze(control, gazePoint))
                 {
-                    if (frameworkElement == gazeControl)
+                    return control;
+                }
+            }
+
+            return null;
+        }
+
+        private bool IsControlUnderGaze(Control control, Point gazePoint)
+        {
+            var uiElements = VisualTreeHelper.FindElementsInHostCoordinates(gazePoint, control, true);
+            foreach (var uiElement in uiElements)
+            {
+                if (uiElement is FrameworkElement frameworkElement)
+                {
+                    if (frameworkElement == control)
                         return true;
                 }
             }
@@ -210,14 +260,14 @@ namespace AmazingUWPToolkit.Gaze
                 {
                     isGazeDwelledCalled = true;
 
-                    gazeControl?.OnGazeDwelled(lastHitPoint);
+                    (currentControlUnderGaze as IGazeControl)?.OnGazeDwelled(currentGazePoint);
                 }
             }
             else
             {
                 currentTimerValue += TIMER_INTERVAL;
 
-                gazeControl?.OnGazeFixationProgressChanged(currentTimerValue);
+                (currentControlUnderGaze as IGazeControl)?.OnGazeFixationProgressChanged(currentTimerValue);
             }
         }
 
--- AmazingUWPToolkit.Gaze/GazeTracker/IGazeTracker.cs
+++ AmazingUWPToolkit.Gaze/GazeTracker/IGazeTracker.cs
@@ -1,8 +1,14 @@
-﻿namespace AmazingUWPToolkit.Gaze
+﻿using Windows.UI.Xaml.Controls;
+
+namespace AmazingUWPToolkit.Gaze
 {
     public interface IGazeTracker
     {
         #region Methods
+
+        bool AddControl(Control control);
+
+        bool RemoveControl(Control control);
 
         void Start();
 
