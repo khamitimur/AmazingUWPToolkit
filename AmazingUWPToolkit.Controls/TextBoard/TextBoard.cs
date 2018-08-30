fe2061c55aeac295fa803589046a82a26ec6e989
Timur Khamidov <baton237@gmail.com>
2018-08-30 19:08:33 +0300

    Update Controls project to use new ExtendedObservableCollection implementation

--- AmazingUWPToolkit.Controls/TextBoard/TextBoard.cs
+++ AmazingUWPToolkit.Controls/TextBoard/TextBoard.cs
@@ -1,4 +1,5 @@
-﻿using JetBrains.Annotations;
+﻿using AmazingUWPToolkit.Collections;
+using JetBrains.Annotations;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
